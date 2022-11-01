e23feb5d277eabfb2c4bc5653778d6cab5fd1160
longvoct <volonglqd@gmail.com>
2022-11-01 22:16:18 +0700

    Update Carousel

--- assets/data/BannerList.js
+++ assets/data/BannerList.js
@@ -1,28 +1,52 @@
+import { Dimensions, Image } from "react-native"
 import banner1 from "../images/banner/banner1.png"
 import banner2 from "../images/banner/banner2.png"
 import banner3 from "../images/banner/banner3.png"
 import banner4 from "../images/banner/banner4.png"
-import banner5 from "../images/banner/banner5.png"
+
+const {width: screenWidth} = Dimensions.get("window");
+
 const BannerList = [
   {
     id: 1,
-    image:banner1,
+    image: <Image source={banner1} 
+                  style={{
+                    width: screenWidth -50 , 
+                    height:200, 
+                    marginHorizontal: 25,
+                    marginLeft: 0,
+                    borderRadius:8
+                    }} resizeMode="cover"/>
   },
   {
     id: 2,
-    image:banner2,
+    image: <Image source={banner2} 
+    style={{
+      width: screenWidth -50 , 
+      height:200, 
+      marginHorizontal: 25,
+      borderRadius:8
+      }} resizeMode="cover"/>
   },
   {
     id: 3,
-    image:banner3,
+    image: <Image source={banner3} 
+    style={{
+      width: screenWidth -50 , 
+      height:200, 
+      marginHorizontal: 25,
+      borderRadius:8
+      }} resizeMode="cover"/>
   },
   {
     id: 4,
-    image:banner4,
-  },
-  {
-    id: 5,
-    image:banner5,
+    image: <Image source={banner4} 
+    style={{
+      width: screenWidth -50 , 
+      height:200, 
+      marginHorizontal: 25,
+      borderRadius:8
+      }} resizeMode="cover"/>
   },
 ]
 export default BannerList;
